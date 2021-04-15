9d5e580b3360cd501f2f736e6eefd15f49c0cae1
Deepak Yadav <56051050+DeepakYadav01@users.noreply.github.com>
2021-04-15 10:52:25 +0530

    Updated API URI

--- src/main/java/com/java/advancejava/webservices/assignment/constants/ApiConstants.java
+++ src/main/java/com/java/advancejava/webservices/assignment/constants/ApiConstants.java
@@ -2,6 +2,7 @@ package com.java.advancejava.webservices.assignment.constants;
 
 public class ApiConstants {
 	//Api URI
+	
 	public static final String GET_ALL_EMPLOYEE = "http://localhost:8080/api/employee/all";
 	public static final String GET_SINGLE_EMPLOYEE = "http://localhost:8080/api/employee/get/";
 	public static final String ADD_EMPLOYEE = "http://localhost:8080/api/employee/add";
